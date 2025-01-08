5be70b902e66205d7778a262306f389f6661aba8
pallandir <pallandir2@gmail.com>
2025-01-08 20:40:43 +0800

    Add 283 with update

--- easy/283_move_zeroes.py
+++ easy/283_move_zeroes.py
@@ -1,5 +1,5 @@
-def moveZeroes(nums: list[int]):
-    i,j = 0,0
+def move_zeroes(nums: list[int]):
+    i, j = 0, 0
 
     while j < len(nums):
         if nums[j] != 0:
@@ -10,6 +10,7 @@ def moveZeroes(nums: list[int]):
 
 if __name__ == "__main__":
     nums = [0, 1, 0, 3, 12]
+    # nums = [2, 1]
     print(nums)
-    moveZeroes(nums)
+    move_zeroes(nums)
     print(nums)
