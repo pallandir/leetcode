6a41205d80d7462c28fc82b8ea690b7838aea5f0
pallandir <pallandir2@gmail.com>
2025-01-22 21:47:47 +0800

    Add preorder traversal

---
+++ sandbox/preorder_tree_traversal.py
@@ -0,0 +1,26 @@
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def preorder_traversal(self):
+        traversed = []
+        stack = [self]
+
+        while stack:
+            current_node = stack.pop()
+            traversed.append(current_node.val)
+            if current_node.right:
+                stack.append(current_node.right)
+            if current_node.left:
+                stack.append(current_node.left)
+        return traversed
+
+
+if __name__ == "__main__":
+    tree = Node(5)
+    tree.left = Node(3)
+    tree.right = Node(2)
+    tree.left.left = Node(6)
+    print(tree.preorder_traversal())
