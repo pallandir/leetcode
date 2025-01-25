339394a66160ae2e80db80d9fc8c688f796082d4
pallandir <pallandir2@gmail.com>
2025-01-25 12:15:12 +0800

    Add state space tree generation for 2 letters word

---
+++ sandbox/two_letters_state_space_tree.py
@@ -0,0 +1,39 @@
+class Node:
+    def __init__(self, value, children=None):
+        self.val = value
+        self.children = children if children else []
+
+    def display_tree(self, level=0):
+        level = []
+        res = []
+        queue = [self]
+
+        while queue:
+            level = []
+            for _ in range(len(queue)):
+                current = queue.pop(0)
+                level.append(current.val)
+                queue.extend(current.children)
+            res.append(level)
+
+        print(res)
+
+
+def state_space_tree(chars_list):
+    def generate_tree(current_word, depth):
+        if depth == 0:
+            return Node(current_word)
+        root = Node(current_word)
+
+        for char in chars_list:
+            child = generate_tree(current_word + char, depth - 1)
+            root.children.append(child)
+        return root
+
+    return generate_tree("", len(chars_list))
+
+
+if __name__ == "__main__":
+    chars = ["a", "b"]
+    tree = state_space_tree(chars)
+    tree.display_tree()
