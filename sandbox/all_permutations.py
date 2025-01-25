9ab77878cb4c8e2ba1cf179182a9331cf598300c
pallandir <pallandir2@gmail.com>
2025-01-25 16:54:09 +0800

    Permutation state space tree

---
+++ sandbox/all_permutations.py
@@ -0,0 +1,26 @@
+def permutations_state_space_tree(values):
+    result = []  # persistent state
+
+    def generate_tree(path, remaining):
+        if not remaining:
+            result.append(
+                path[:]
+            )  # shallow copy of the list (otherwise it would be changed by bactracking call)
+
+        for index, value in enumerate(remaining):
+            path.append(value)
+            generate_tree(
+                path, remaining[:index] + remaining[index + 1 :]
+            )  # remove the current value from remaining for next choice
+            path.pop()
+
+        return result
+
+    return generate_tree([], values)
+
+
+if __name__ == "__main__":
+    values = [1, 2, 3]
+    tree = permutations_state_space_tree(values)
+    for value in tree:
+        print(f"{value}")
