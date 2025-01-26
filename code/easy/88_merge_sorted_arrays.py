75866b92c59f83838a8dbfcd2e41d83326faeecd
pallandir <pallandir2@gmail.com>
2025-01-26 18:25:56 +0800

    Fix remove useless code in mege sorted array

--- code/easy/88_merge_sorted_arrays.py
+++ code/easy/88_merge_sorted_arrays.py
@@ -1,13 +1,9 @@
 # complexity : concat : O(m+n) + sort O(log(m+n))
-def merge_array(array1, array2, m, n):
+def merge_sorted_array(array1, array2, m, n):
     array1[:] = sorted(array1[:m] + array2[:n])
 
 
-def merge_array_2(array1, array2, m, n):
-    array1[m:] = array2[:n]
-
-
-def merge_array_3(array1, array2, m, n):
+def merge_sorted_array_3(array1, array2, m, n):
     i, j, k = m - 1, n - 1, m + n - 1
 
     while j >= 0:
@@ -25,6 +21,5 @@ if __name__ == "__main__":
     m = 3
     nums2 = [2, 5, 6]
     n = 3
-    merge_array_3(nums1, nums2, m, n)
-
+    merge_sorted_array_3(nums1, nums2, m, n)
     print(nums1)
