7ca9f611e4c30a44536006c2ef9ab29573899941
pallandir <pallandir2@gmail.com>
2025-01-26 18:27:04 +0800

    Clean contains duplicate problem

--- code/easy/219_contains_duplicate_2.py
+++ code/easy/219_contains_duplicate_2.py
@@ -1,4 +1,4 @@
-def containsDuplicates2(nums: list, k: str):
+def contains_duplicates(nums: list, k: str):
     hash_map = {}
 
     for index, value in enumerate(nums):
@@ -10,7 +10,4 @@ def containsDuplicates2(nums: list, k: str):
 
 
 if __name__ == "__main__":
-    nums = [1, 2, 3, 1]
-    k = 3
-
-    print(containsDuplicates2(nums, k))
+    print(contains_duplicates([1, 2, 3, 1], 3))
