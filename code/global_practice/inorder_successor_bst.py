db405ed63de56f9fab85d44a6cab8b8d7b4579cc
pallandir <pallandir2@gmail.com>
2025-02-10 20:52:36 +0800

    Work on BST

---
+++ code/global_practice/inorder_successor_bst.py
@@ -0,0 +1,28 @@
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def inorder_successor(root, node):
+    successor = None
+
+    while root:
+        if node.val < root.val:
+            # the current root could be the right successor but one on the left could also be smaller and meet the condition
+            successor = root
+            root = root.left
+        else:
+            root = root.right
+    return successor.val
+
+
+if __name__ == "__main__":
+    tree = Node(20)
+    tree.left = Node(10)
+    tree.right = Node(30)
+    tree.left.left = Node(8)
+    tree.left.right = Node(15)
+
+    print(inorder_successor(tree, Node(10)))
