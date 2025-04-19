5b94bce1bc4072c4e0c8a74f9416e1689ac94a18
pallandir <pallandir2@gmail>
2025-04-19 21:16:43 +0800

    Greedy florist (greedy) fix logic

--- code/greedy/greedy_florist.py
+++ code/greedy/greedy_florist.py
@@ -1,12 +1,14 @@
 def greedy_florist(flowers, friends):
-    base_sum = sum(flowers[-friends:])
-    for index in range(len(flowers) - friends):
-        base_sum += ((index + 1) + 1) * flowers[index]
+    flowers.sort(reverse=True)
+    total_cost = 0
+    for index in range(len(flowers)):
+        total_cost += index // friends + 1 * flowers[index]
 
-    return base_sum
+    return total_cost
 
 
 if __name__ == "__main__":
     print(greedy_florist([1, 2, 3, 4], 3))
     print(greedy_florist([2, 5, 6], 3))
     print(greedy_florist([2, 5, 6], 2))
+    print(greedy_florist([1, 3, 5, 7, 9], 2))
