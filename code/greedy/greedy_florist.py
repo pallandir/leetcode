b4cfb3c86eb579afc1200a34cd913e84fd5f6bf6
pallandir <pallandir2@gmail>
2025-04-20 10:26:59 +0800

    Update greedy florists

--- code/greedy/greedy_florist.py
+++ code/greedy/greedy_florist.py
@@ -2,7 +2,7 @@ def greedy_florist(flowers, friends):
     flowers.sort(reverse=True)
     total_cost = 0
     for index in range(len(flowers)):
-        total_cost += index // friends + 1 * flowers[index]
+        total_cost += (index // friends + 1) * flowers[index]
 
     return total_cost
 
